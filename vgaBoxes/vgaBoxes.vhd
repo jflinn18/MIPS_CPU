ca83a7a39ff46c0c5c8e55c07a2b85beacc4cab9
Derek Hansen <dhansen17@my.whitworth.edu>
2017-05-16 15:33:37 -0700

    Currently created clock, need to create the bricks for coloration

--- vgaBoxes/vgaBoxes.vhd
+++ vgaBoxes/vgaBoxes.vhd
@@ -13,7 +13,13 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity vgaBoxes is
+entity vgaBoxes is port(
+clk50_in : in std_logic;
+red_out : out std_logic_vector(2 downto 0);
+green_out : out std_logic_vector(2 downto 0);
+blue_out : out std_logic_vector(2 downto 0);
+hs_out : out std_logic;
+vs_out : out std_logic);
 end vgaBoxes;
 
 architecture Behavioral of vgaBoxes is
@@ -24,6 +30,7 @@ architecture Behavioral of vgaBoxes is
 		   blue_out:   out STD_LOGIC_VECTOR(2 downto 0)
 	 );
   end component;
+
 
   signal green_out1: out STD_LOGIC_VECTOR(2 downto 0);
   signal green_out2: out STD_LOGIC_VECTOR(2 downto 0);
@@ -51,14 +58,34 @@ architecture Behavioral of vgaBoxes is
   signal blue_out6: out STD_LOGIC_VECTOR(2 downto 0);
   signal blue_out7: out STD_LOGIC_VECTOR(2 downto 0);
   signal blue_out8: out STD_LOGIC_VECTOR(2 downto 0);
+  
+  signal clk25  	 : std_logic
+  signal hcounter  : integer range 0 to 800;
+  signal vcounter  : integer range o to 521;
+  
 
 
 begin
-
 -- We need to connect to vgamem in the MIPS cpu
 --    This will be with some sort of clock that
 --    is significantly slower than the MIPS clock (clk25)
 
+-- generate a 25Mhz clock
+  process (clk50_in)
+  begin
+    if clk50_in'event and clk50_in='1' then
+      clk25 <= not clk25;
+    end if;
+  end process;
+
+
+  p2: process(clk25, hcounter, vcounter)
+			variable brick_height : integer := 170;
+	variable brick_width  : integer := 20;
+	variable brick0_left : integer := 50;
+	variable brick0_right : integer;
+
+	
 box1: port map(vgamem(0), green_out1, red_out1, blue_out1);
 box2: port map(vgamem(1), green_out2, red_out2, blue_out2);
 box3: port map(vgamem(2), green_out3, red_out3, blue_out3);
