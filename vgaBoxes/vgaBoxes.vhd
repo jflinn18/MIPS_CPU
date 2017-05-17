81072acca309ca8254ff1e0a3f8dd8e50c93946d
Joseph <jflinn18@my.whitworth.edu>
2017-05-17 08:38:45 -0700

    fixed some VHDL mistakes

--- vgaBoxes/vgaBoxes.vhd
+++ vgaBoxes/vgaBoxes.vhd
@@ -32,34 +32,34 @@ architecture Behavioral of vgaBoxes is
   end component;
 
 
-  signal green_out1: out STD_LOGIC_VECTOR(2 downto 0);
-  signal green_out2: out STD_LOGIC_VECTOR(2 downto 0);
-  signal green_out3: out STD_LOGIC_VECTOR(2 downto 0);
-  signal green_out4: out STD_LOGIC_VECTOR(2 downto 0);
-  signal green_out5: out STD_LOGIC_VECTOR(2 downto 0);
-  signal green_out6: out STD_LOGIC_VECTOR(2 downto 0);
-  signal green_out7: out STD_LOGIC_VECTOR(2 downto 0);
-  signal green_out8: out STD_LOGIC_VECTOR(2 downto 0);
+  signal green_out1: STD_LOGIC_VECTOR(2 downto 0);
+  signal green_out2: STD_LOGIC_VECTOR(2 downto 0);
+  signal green_out3: STD_LOGIC_VECTOR(2 downto 0);
+  signal green_out4: STD_LOGIC_VECTOR(2 downto 0);
+  --signal green_out5: STD_LOGIC_VECTOR(2 downto 0);
+  --signal green_out6: STD_LOGIC_VECTOR(2 downto 0);
+  --signal green_out7: STD_LOGIC_VECTOR(2 downto 0);
+  --signal green_out8: STD_LOGIC_VECTOR(2 downto 0);
 
-  signal red_out1: out STD_LOGIC_VECTOR(2 downto 0);
-  signal red_out2: out STD_LOGIC_VECTOR(2 downto 0);
-  signal red_out3: out STD_LOGIC_VECTOR(2 downto 0);
-  signal red_out4: out STD_LOGIC_VECTOR(2 downto 0);
-  signal red_out5: out STD_LOGIC_VECTOR(2 downto 0);
-  signal red_out6: out STD_LOGIC_VECTOR(2 downto 0);
-  signal red_out7: out STD_LOGIC_VECTOR(2 downto 0);
-  signal red_out8: out STD_LOGIC_VECTOR(2 downto 0);
+  signal red_out1: STD_LOGIC_VECTOR(2 downto 0);
+  signal red_out2: STD_LOGIC_VECTOR(2 downto 0);
+  signal red_out3: STD_LOGIC_VECTOR(2 downto 0);
+  signal red_out4: STD_LOGIC_VECTOR(2 downto 0);
+  --signal red_out5: STD_LOGIC_VECTOR(2 downto 0);
+  --signal red_out6: STD_LOGIC_VECTOR(2 downto 0);
+  --signal red_out7: STD_LOGIC_VECTOR(2 downto 0);
+  --signal red_out8: STD_LOGIC_VECTOR(2 downto 0);
 
-  signal blue_out1: out STD_LOGIC_VECTOR(2 downto 0);
-  signal blue_out2: out STD_LOGIC_VECTOR(2 downto 0);
-  signal blue_out3: out STD_LOGIC_VECTOR(2 downto 0);
-  signal blue_out4: out STD_LOGIC_VECTOR(2 downto 0);
-  signal blue_out5: out STD_LOGIC_VECTOR(2 downto 0);
-  signal blue_out6: out STD_LOGIC_VECTOR(2 downto 0);
-  signal blue_out7: out STD_LOGIC_VECTOR(2 downto 0);
-  signal blue_out8: out STD_LOGIC_VECTOR(2 downto 0);
+  signal blue_out1: STD_LOGIC_VECTOR(2 downto 0);
+  signal blue_out2: STD_LOGIC_VECTOR(2 downto 0);
+  signal blue_out3: STD_LOGIC_VECTOR(2 downto 0);
+  signal blue_out4: STD_LOGIC_VECTOR(2 downto 0);
+  --signal blue_out5: STD_LOGIC_VECTOR(2 downto 0);
+  --signal blue_out6: STD_LOGIC_VECTOR(2 downto 0);
+  --signal blue_out7: STD_LOGIC_VECTOR(2 downto 0);
+  --signal blue_out8: STD_LOGIC_VECTOR(2 downto 0);
   
-  signal clk25  	 : std_logic
+  signal clk25  	 : std_logic;
   signal hcounter  : integer range 0 to 800;
   signal vcounter  : integer range o to 521;
   
@@ -90,10 +90,11 @@ box1: port map(vgamem(0), green_out1, red_out1, blue_out1);
 box2: port map(vgamem(1), green_out2, red_out2, blue_out2);
 box3: port map(vgamem(2), green_out3, red_out3, blue_out3);
 box4: port map(vgamem(3), green_out4, red_out4, blue_out4);
-box5: port map(vgamem(4), green_out5, red_out5, blue_out5);
-box6: port map(vgamem(5), green_out6, red_out6, blue_out6);
-box7: port map(vgamem(6), green_out7, red_out7, blue_out7);
-box8: port map(vgamem(7), green_out8, red_out8, blue_out8);
+
+--box5: port map(vgamem(4), green_out5, red_out5, blue_out5);
+--box6: port map(vgamem(5), green_out6, red_out6, blue_out6);
+--box7: port map(vgamem(6), green_out7, red_out7, blue_out7);
+--box8: port map(vgamem(7), green_out8, red_out8, blue_out8);
 
 
 -- We need to display the boxes on the screen. Derek will do this
